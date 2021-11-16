11c8c0d06ba2f3e92001ae0541ca8e1e3d832378
OussamaEchi99 <oussama.echi99@gmail.com>
2021-11-16 16:49:01 +0100

    Finito

---
+++ MILESTONE 2/js/script.js
@@ -0,0 +1,28 @@
+// Per i multipli di 3 stampi in console “Fizz” al posto del numero 
+// e per i multipli di 5 stampi in console Buzz.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi
+// in console FizzBuzz.
+
+// Scrivo in console i numeri da 1 a 1000, poi ogni multiplo
+// di 3 scrivo izz e ogni multiplo di 5 scrivo Buzz.
+const boxContainer = document.querySelector('.container');
+
+for(let i = 1; i <= 100; i++ ) {
+
+    let fizzOrBuzz;
+    if (( i % 3 === 0 ) && ( i % 5 === 0 )) {
+        fizzOrBuzz = 'FizzBuzz'
+    } else if( i % 3 === 0 ) {
+        fizzOrBuzz = 'Fizz'
+    } else if( i % 5 === 0 ) {
+        fizzOrBuzz = 'Buzz'
+    } else {
+        fizzOrBuzz = i
+    }
+
+    const newBox = `
+        <div class="box">${fizzOrBuzz}</div>
+    `;
+
+    boxContainer.innerHTML += newBox;
+}
